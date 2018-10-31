3fe9e2c1bd9c213bb070d71f9f150fae2b8af038
ktochow1 <ktochow1@binghamton.edu>
2018-10-31 22:52:25 +0000

    automatically backed up by learn

--- lib/book.rb
+++ lib/book.rb
@@ -22,6 +22,14 @@ class Book
   def page_count
     @page_count
   end
+  
+  def genre=(genre)
+    @genre = genre
+  end
+  
+  def genre
+    @genre
+  end
 end
 
 #Book.new("And Then There Were None")
